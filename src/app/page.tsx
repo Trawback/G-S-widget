2bbae59236e328fadf59a76fe6ce5ac9cf7f7026
Trawback <sebastianserv8@gmail.com>
2025-08-14 16:10:16 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import LuxuryTransportWidget from '@/components/LuxuryTransportWidget';
-import GoogleMapsTest from '@/components/GoogleMapsTest';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black p-4 md:p-8">
       <div className="container mx-auto">
-        <GoogleMapsTest />
         <LuxuryTransportWidget />
       </div>
     </main>
